1092b43ba7f83ffa3f1a04b91648f09b2724e75c
alexkomaha <alexkomakha@gmail.com>
2022-08-12 15:01:54 +0900

    rails

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+require "open-uri"
+
+article.photo.attach(...)
+has_one_attached :photo
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+article = Article.new(title: "NES", body: "A great console")
+article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.save
